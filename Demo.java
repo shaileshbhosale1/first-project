5e0e1287e318ba37d8ad2e72f51a6cf13e326554
shaileshbhosale1 <shaileshbhosale2004@outlook.com>
2023-03-05 00:27:45 +0530

    java

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo{
+    public static void main(String[] args){
+        Demo d = new Demo();
+        d.sum();
+        d.sum(4);
+    }
+
+void sum(){
+    System.out.println("hello");
+
+}
+ void sum(int a){
+     System.out.println("hi =" + a);
+}
+}
---
+++ main.java
@@ -0,0 +1,5 @@
+public class main{
+    public static void main(String[] args){
+        System.out.println("package system does not exist error solved");
+    }
+}
