caf2efe85e33fbd5da89960705f1b3d0f1dfba3b
shaileshbhosale1 <shaileshbhosale2004@outlook.com>
2023-03-07 11:32:48 +0530

    admin.java

---
+++ admin.java
@@ -0,0 +1,27 @@
+public class admin{
+    public static void main(String[] args){
+        admin a = new admin();
+
+        a.login("magic","magic124");
+        a.login(567,153);
+        
+
+         
+
+
+    }
+    void login(String UID,String PWD ){
+        System.out.println("loggedin to amazon");
+    
+    }
+    void login(int phoneNo,int OTP){
+        System.out.println("loggedin to flipkart");
+    }
+
+        
+    }
+
+
+
+
+    
