7962785c946079bd80d6900188923a70c51886a5
shaileshbhosale1 <shaileshbhosale2004@outlook.com>
2023-03-14 09:55:08 +0530

    change java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+    public static void main(String[] args){
+        int a=10;
+        int b=25;
+        int sum=a + b;
+        int diff=a-b;
+        System.out.println(sum );
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+    }
+}
