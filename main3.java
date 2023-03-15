7290ffb026556351ebdd758a7639ea0f070da5fe
shaileshbhosale1 <shaileshbhosale2004@outlook.com>
2023-03-15 18:52:45 +0530

    CHANGE

---
+++ main3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String name=sc.next();
+        System.out.println(name);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+
+    }
+    
+}
